5f7035d0139f932c0d58b15f6082eadcc3568a50
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-18 10:23:08 -0400

    add tables for project data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def dashboard
     @project_count = Project.count
-    @open_source_count = Project.where(open_source: true).count
-    @government_wide_reuse_count = Project.where(government_wide_reuse: true).count
+    @open_source_count = Project.where(open_source: Project::OPEN_SOURCE).count
+    @closed_source_count = @project_count - @open_source_count
+    @gov_reuse_count = Project.where(government_wide_reuse: Project::GOVERNMENT_WIDE_REUSE).count
+    @non_gov_reuse_count = @project_count - @gov_reuse_count
   end
 
   def help
