0ff9e71cd25e9a6330adfca5d0aa853fa30b7a35
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-20 11:46:57 -0400

    generic chart generation

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,29 +1,26 @@
 $(document).on("turbolinks:load", function () {
 
   if (CodeCentral.page() === "pages#dashboard") {
-    var createLegend = function (chart) {
-      var html = [];
-      html.push('<div>');
-      for (var i = 0; i < chart.data.datasets[0].data.length; i++) {
-        html.push('<div>');
-        html.push('<span class="chart-legend-color" style="background-color: ' + chart.data.datasets[0].backgroundColor[i] + '">&nbsp;</span>');
-        html.push(chart.data.labels[i]);
-        html.push('</div>');
-      }
-      html.push('</div>');
-      return html.join("");
-    };
 
-    var chart = function (ctx, labels, data) {
+    var chart = function (datasetName) {
+      var $datasetSource = $("table[data-dataset='" + datasetName + "']");
+      var $canvas = $(".chart[data-dataset='" + datasetName + "']");
       var backgroundColor = ["#4773aa", "#f9c642"];
-      return new Chart(ctx, {
-        type: "pie",
+      var labels = [];
+      var values = [];
+      $datasetSource.find(".chart-label").each(function () {
+        labels.push($(this).text());
+      });
+      $datasetSource.find(".chart-value").each(function () {
+        values.push($(this).text());
+      });
+      return new Chart($canvas, {
+        type: "doughnut",
         data: {
           labels: labels,
-          datasets: [{ data: data, backgroundColor: backgroundColor }]
+          datasets: [{ data: values, backgroundColor: backgroundColor }]
         },
         options: {
-          legendCallback: createLegend,
           legend: {
             display: false
           }
@@ -31,15 +28,11 @@ $(document).on("turbolinks:load", function () {
       });
     };
 
-    var opennessChart = chart($("#project-openness-chart"),
-      [$("#open-source-label").text(), $("#closed-source-label").text()],
-      [$("#open-source-value").text(), $("#closed-source-value").text()]);
-    $("#project-openness-legend").html(opennessChart.generateLegend());
+    $(".chart").each(function () {
+      var datasetName = $(this).data("dataset");
+      chart(datasetName);
+    });
 
-    var reuseChart = chart($("#project-reuse-chart"),
-      [$("#reuse-label").text(), $("#non-reuse-label").text()],
-      [$("#reuse-value").text(), $("#non-reuse-value").text()]);
-    $("#project-reuse-legend").html(reuseChart.generateLegend());
   }
 
 });
