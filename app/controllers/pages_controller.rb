c67d459dc16b8c3bc0de366f4ff45ce32f736834
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-20 23:17:31 -0400

    handle condition where there are no projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   def dashboard
     @project_count = Project.count
     @open_source_count = Project.where(open_source: Project::OPEN_SOURCE).count
+    @open_source_percentage = @project_count == 0 ? 0 : 100 * @open_source_count / @project_count.to_f
     @closed_source_count = @project_count - @open_source_count
+    @closed_source_percentage = @project_count == 0 ? 0 : 100 * @closed_source_count / @project_count.to_f
     @gov_reuse_count = Project.where(government_wide_reuse: Project::GOVERNMENT_WIDE_REUSE).count
+    @gov_reuse_percentage = @project_count == 0 ? 0 : 100 * @gov_reuse_count / @project_count.to_f
     @non_gov_reuse_count = @project_count - @gov_reuse_count
+    @non_gov_reuse_percentage = @project_count == 0 ? 0 : 100 * @non_gov_reuse_count / @project_count.to_f
   end
 
   def help
